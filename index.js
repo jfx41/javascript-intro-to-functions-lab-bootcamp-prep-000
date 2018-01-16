62912a3dd367143cf53042e72b94b7a21d581067
Kuzuri Ao <online@nahlem.com>
2018-01-16 15:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
 function whisper(string) {
   return string.toLowerCase()
 }
